a9ac508ecf989223550467519ba204972a7794f0
nicholasdavies <n.g.davies@gmail.com>
2024-01-04 23:32:55 +0000

    Allow multi-line input in lua_shell

--- R/lua_shell.R
+++ R/lua_shell.R
@@ -16,24 +16,45 @@ lua_shell = function(L = NULL)
     # See if crayon package is available
     crayon_available = requireNamespace("crayon", quietly = TRUE);
 
-    # Error reporting
-    do_error = function(e)
-    {
-        if (crayon_available) {
-            cat(crayon::red(e$message), "\n")
-        } else {
-            cat(e$message, "\n")
-        }
-    }
-
     # Loop until blank line received
     line = "."
+    prev_line = ""
     while (line != "")
     {
         # Read a line
-        line = readline("lua > ")
+        if (prev_line == "") {
+            line = readline("lua > ")
+        } else {
+            # If we're building up multi-line input, show a special prompt
+            line = readline(" +    ")
+            if (line == "") {
+                # Exit multi-line if nothing entered
+                line = "."
+                prev_line = ""
+                next
+            } else {
+                # Otherwise build up input
+                line = paste(prev_line, line)
+            }
+        }
 
-        # Try to execute the line, print error message if error thrown
-        tryCatch(lua(line, L = L), error = do_error)
+        # Try to execute the line, capturing any error
+        err = tryCatch(lua(line, L = L), error = function(e) e)
+
+        # If there is an error:
+        if (!is.null(err)) {
+            if (grepl("'<eof>'$", err$message)) {
+                # Unexpected end of input: try to gather more input
+                prev_line = line
+                next
+            } else if (crayon_available) {
+                # Other error: show error message in red if possible
+                cat(crayon::red(err$message), "\n")
+            } else {
+                # Or just in normal colour if not possible
+                cat(err$message, "\n")
+            }
+        }
+        prev_line = ""
     }
 }
