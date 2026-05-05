008e4fdfc5b03aa41089e5f520837abe813ab74b
Nick Davies <n.g.davies@gmail.com>
2026-05-05 16:07:37 +0100

    fix Windows R.dll path for R >= 4.2.0

--- R/zzz.R
+++ R/zzz.R
@@ -6,7 +6,14 @@
         # Path to package dylib for LuaJIT FFI
         getLoadedDLLs()[["luajr"]][["path"]],
         # Path to R shared library (only used on Windows)
-        if (.Platform$OS.type == "windows") file.path(R.home("bin"), .Platform$r_arch, "R.dll") else "",
+        if (.Platform$OS.type == "windows") {
+            # R 4.2+ dropped multi-arch: R.home("bin") already includes arch.
+            # R 4.0-4.1 had R.dll in R.home("bin")/r_arch/.
+            if (getRversion() >= "4.2.0")
+                file.path(R.home("bin"), "R.dll")
+            else
+                file.path(R.home("bin"), .Platform$r_arch, "R.dll")
+        } else "",
         # Path to luajr module
         system.file("Lua", "luajr.lua", package = "luajr"),
         # Path to R module
