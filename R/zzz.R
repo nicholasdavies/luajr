9071cd5f48598bbd0d75dfbde6d8e686f7bdd771
Nick Davies <n.g.davies@gmail.com>
2026-05-09 16:55:50 +0100

    another windows fix attempt

--- R/zzz.R
+++ R/zzz.R
@@ -5,14 +5,8 @@
         getRversion()[[1, 1:3]],
         # Path to package dylib for LuaJIT FFI
         getLoadedDLLs()[["luajr"]][["path"]],
-        # Path to R shared library (only used on Windows)
-        if (.Platform$OS.type == "windows") {
-            if (getRversion() >= "4.2.0") {
-                file.path(R.home("bin"), "R.dll")
-            } else {
-                file.path(R.home("bin"), .Platform$r_arch, "R.dll")
-            }
-        } else "",
+        # Path to R shared library (only used on Windows).
+        if (.Platform$OS.type == "windows") file.path(R.home("bin"), "R.dll") else "",
         # Path to luajr module
         system.file("Lua", "luajr.lua", package = "luajr"),
         # Path to R module
